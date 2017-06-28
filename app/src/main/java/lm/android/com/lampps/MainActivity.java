acabcc305966ac9dd44bfd56a24c112b2e0506aa
hosein.motlagh <ali.highboy@yahoo.com>
2017-06-28 06:21:44 +0430

    a1 done

--- app/src/main/java/lm/android/com/lampps/MainActivity.java
+++ app/src/main/java/lm/android/com/lampps/MainActivity.java
@@ -2,6 +2,7 @@ package lm.android.com.lampps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("a1","a1");
     }
 }
